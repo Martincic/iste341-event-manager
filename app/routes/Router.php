a130d46b4953c05a8a6c32e326207b58d748728e
Martincic <martincic.tomas@gmail.com>
2021-10-02 21:57:04 +0200

    added protected routes

--- app/routes/Router.php
+++ app/routes/Router.php
@@ -9,8 +9,14 @@ class Router {
      * Handles incoming requests. A query parameter 'page' is used to identify 
      * the view that will be rendered and returned back to the client.
      */
-    public static function handleRequest() {
-
+    public static function handleRequest() 
+    {
+        //from www.google.com/events/123 
+        //the slug would be events/123
+        
+        //uncomment line below to set yourself as admin
+        //$_SESSION['user']['role'] = 'admin';
+        
         $slug = self::getSlug();
         
         if('' == $slug) {
@@ -21,7 +27,12 @@ class Router {
             HomeController::index();
         }
         
+        /*
+            This is an example of protected route.
+            (Route where user must be logged in)
+        */
         if('admin' == $slug) {
+            self::protectRoute('admin');
             HomeController::admin();
         }
         
@@ -47,6 +58,33 @@ class Router {
         $view->render([]);
     }
 
+    /*
+        This checks if user is logged in, if no parameter is required it will 
+        look only if user is logged in. If there is parameter, it will check 
+        if user is of that exact role.
+    */
+    public static function protectRoute(string $role = null)
+    {
+        //if user exists
+        if(isset($_SESSION['user'])){
+
+            //if role is not set, continue
+            if(!$role) return;
+
+            //if role is set and matches, continue
+            if($_SESSION['user']['role'] == $role) {
+                return; //continue
+            }
+            else self::abort();
+        }
+        else self::abort();
+    }
+
+    public static function abort()
+    {
+        $view = new View('app/view/pages/login.php');
+        $view->render([]);
+    }
     /*
         .htaccess points any request to router.php (here),
     */
