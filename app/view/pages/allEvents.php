1c28c38f55edfe39bb7ae5ecf83ace749689ff76
soguic <so6561@g.rit.edu>
2021-10-04 19:17:24 +0200

    allEvents edit

--- app/view/pages/allEvents.php
+++ app/view/pages/allEvents.php
@@ -1,17 +1,26 @@
 <?php include 'app/view/inc/navbar.php';?>
 <div class="container">
-    <div class="row">
+    <div class="row allEvents">
         <div class="col-md-12">
             <h1>ALL EVENTS</h1>
+            <p class="text-center">Scroll down to view events</p>
+        </div>
+        <div class="col-md-12 text-center">
+        <img src="public/img/fast-forward.png" width="32" height="60" class="downIcon" />
+
         </div>
     </div>
-    
-    <ol class="list-group list-group-numbered">
-        <?php 
+    <div class="row">
+        <div class="col-md-12">
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <?php 
 
-            foreach ($data as $event) {
-                include('components/single_event_item.php');
-            }
-        ?>
-    </ol>
+                foreach ($data as $event) {
+                    include('components/single_event_item.php');
+                }
+            ?>
+        </div>
+        <div class="swiper-pagination"></div>
+    </div></div></div>
 </div>
--- app/view/pages/welcome.php
+++ app/view/pages/welcome.php
@@ -4,28 +4,3 @@
             <p>To navigate around this website, use URLs as follows:</p>
         </form>
 
-        <div class="swiper mySwiper">
-            <div class="swiper-wrapper">
-
-                <!-- dodat u for each $event/$session etc -->
-                <div class="swiper-slide">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-                                Slide 1
-                            </div>
-                        </div>       
-                    </div>
-                </div>
-        
-                <div class="swiper-slide">Slide 2</div>
-                <div class="swiper-slide">Slide 3</div>
-                <div class="swiper-slide">Slide 4</div>
-                <div class="swiper-slide">Slide 5</div>
-                <div class="swiper-slide">Slide 6</div>
-                <div class="swiper-slide">Slide 7</div>
-                <div class="swiper-slide">Slide 8</div>
-                <div class="swiper-slide">Slide 9</div>
-            </div>
-            <div class="swiper-pagination"></div>
-        </div>
