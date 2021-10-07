6e16db883d0fb80a6f7828514eadddee8a35e20e
soguic <so6561@g.rit.edu>
2021-10-07 18:42:59 +0200

    add registrations v1

--- app/view/pages/registrations.php
+++ app/view/pages/registrations.php
@@ -1,11 +1,9 @@
 <?php include 'app/view/inc/navbar.php';?>
-<div class="container">
-    <div class="row hero">
+<div class="container hero">
+    <div class="row">
         <div class="col-md-12">
             <h1>REGISTRATIONS</h1>
-            <!-- IF THERE ARE ANY REGISTRATIONS FOR THIS USER -->
             <p class="text-center">Scroll down to view your registrations</p>
-            <!-- ENDIF -->
         </div>
         <div class="col-md-12 text-center">
         <img src="public/img/fast-forward.png" width="32" height="60" class="downIcon" />
@@ -14,22 +12,26 @@
     </div>
     <div class="row content">
         <div class="col-md-12">
-            <!-- IF THERE ARE ANY REGISTRATIONS FOR THIS USER -> SHOW LIST -->
+        <?php 
+        if(count($data['events']) > 0){
+            echo '
             <div class="swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <?php 
+                <div class="swiper-wrapper">';
+                foreach ($data['events'] as $event) {
+                    include(__DIR__.'/../components/single_registration_item.php');
+                }
 
-                        foreach ($data as $event) {
-                            include(__DIR__.'/../components/single_event_item.php');
-                        }
-                    ?>
-                </div>
+            echo '</div>
                 <div class="swiper-pagination"></div>
-            </div>
-            <!-- ELSE -->
-            <!-- <p>Register for events or sessions using:</p>
-            <p><a href="<?#php echo BASE_URL ?>/events">>Link to allEvents.php</a></p> -->
+            </div>';
 
+        }else{
+            echo '
+            <p>No registrations found!</p>
+            <p>Register for events or sessions using:</p>
+            <p><a href=BASE_URL/events>Link to all Events</a></p>';
+        }
+        ?>
         </div>
     </div>
 </div>
